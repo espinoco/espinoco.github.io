153aa9142c7c0a02805c69a8d86de2309b1eaed0
croespino <6217775+croespino@users.noreply.github.com>
2023-04-02 12:42:00 -0700

    move pages source to the src directory

--- _config.ts
+++ _config.ts
@@ -1,13 +1,13 @@
 import lume from "lume/mod.ts";
 
 const site = lume({
+  src: "./src",
+  // GitHub Pages only allows the / (root) or /docs folders to be selected to build the site
+  dest: "./docs",
   server: {
     page404: "/index.html"
-  },
-  // GitHub Pages only allows the / (root) or /docs folders to be selected to build the site
-  dest: "./docs"
+  }
 });
-site.ignore("README.md");
 site.copy("CNAME");
 
 export default site;
