9093551f6b320234a6c68bee1ee942ce43e3b105
croespino <6217775+croespino@users.noreply.github.com>
2023-04-03 15:12:07 -0700

    add syntax highlight

--- _config.ts
+++ _config.ts
@@ -1,5 +1,9 @@
 import lume from "lume/mod.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
+
+import lang_bash from "https://unpkg.com/@highlightjs/cdn-assets@11.6.0/es/languages/bash.min.js";
+import lang_vimscript from "https://unpkg.com/@highlightjs/cdn-assets@11.6.0/es/languages/vim.min.js";
 
 const site = lume({
   src: "./src",
@@ -11,5 +15,14 @@ const site = lume({
 });
 site.copy("CNAME");
 site.use(slugifyUrls()); // Slugify all page URLs to remove potentially conflicting characters
+site.use(
+  codeHighlight({
+    languages: {
+      bash: lang_bash,
+      vimscript: lang_vimscript
+    }
+  })
+);
+site.copy("css");
 
 export default site;
